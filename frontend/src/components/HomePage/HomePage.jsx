a4171ca62fd01aba14e116fe8525e0fd8429ea99
Evgeniy3 <90893731+Evgeniy3@users.noreply.github.com>
2022-11-16 20:39:05 +0500

    fixed files

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -26,7 +26,7 @@ const HomePage = () => {
   const correctChat = dataChannels.filter((item) => item.id === dataСurrentID);
   const correctChatName = correctChat[0]?.name;
   // eslint-disable-next-line no-debugger
-  debugger;
+  // debugger;
   return (
     <div className="container h-100 my-4 overflow-hidden rounded shadow">
       <div className="row h-100 bg-white flex-md-row">
